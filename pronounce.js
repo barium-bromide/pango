bd7a77bee5f165cc99d2d827c46561dc0315a45e
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2023-10-14 23:15:02 +0800

    arrange the json file

--- pronounce.js
+++ pronounce.js
@@ -11,16 +11,25 @@ fetch("pronounce.json")
       cell.innerHTML = key;
       cell.dataset.cell = "consonant";
 
-      Object.entries(pronounces[key]).forEach(([cellidx, value]) => {
+      Object.entries(pronounces[key]).forEach(([cellidx, info]) => {
         const cell = row.insertCell();
         cell.innerHTML = cellidx;
         cell.dataset.cell = "merge";
         cell.className = "show-info";
 
         const div = document.createElement("div");
-        div.innerHTML = value;
         div.className = "info hide";
 
+        const values = Object.entries(info)
+          .map(([tone, type]) => {
+            const typeEntries = Object.entries(type)
+              .map(([wordClass, meaning]) => `${wordClass}: ${meaning}`)
+              .join('<br>');
+            return `<b>${tone}</b><br>${typeEntries}`;
+          })
+          .join('<br>');
+
+        div.innerHTML = values;
         cell.onclick = () => div.classList.toggle("hide");
         cell.appendChild(div);
       });
